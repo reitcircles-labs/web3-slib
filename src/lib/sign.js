33c924e84dccf63850dee1f9bb4e04936a46bfca
Nilay Saha <saha.nilay@gmail.com>
2024-01-12 00:27:07 +0100

    do not make array of msg

--- src/lib/sign.js
+++ src/lib/sign.js
@@ -8,64 +8,65 @@ import { timeSpan } from './timespan';
  */
 export const sign = async (signer, expires_in = '1d', body = {}) => {
 
-  const expires_in_date = timeSpan(expires_in);
+    const expires_in_date = timeSpan(expires_in);
 
-  validateInput(body);
+    validateInput(body);
 
-  const data = {
-    'Web3-Token-Version': 1,
-    'Expire-Date': expires_in_date,
-    ...body,
-  };
+    const data = {
+        'Web3-Token-Version': 1,
+        'Expire-Date': expires_in_date,
+        ...body,
+    };
 
     
-  const msg = buildMessage(data);
-  
-  if(typeof signer === 'function') {
-    var signObj = await signer(msg);
-  } else {
-    throw new Error('"signer" argument should be a function that returns a signature eg: "msg => web3.eth.personal.sign(msg, <YOUR_ADDRESS>)"')
-  }
-  
-  if(typeof signObj.signature !== 'string') {
-    throw new Error('"signer" argument should be a function that returns a signature string (Promise<string>)')
-  }
+    //const msg = buildMessage(data);
+    const msg = JSON.stringify(data)
+    
+    if(typeof signer === 'function') {
+        var signObj = await signer(msg);
+    } else {
+        throw new Error('"signer" argument should be a function that returns a signature eg: "msg => web3.eth.personal.sign(msg, <YOUR_ADDRESS>)"')
+    }
+    
+    if(typeof signObj.signature !== 'string') {
+        throw new Error('"signer" argument should be a function that returns a signature string (Promise<string>)')
+    }
 
-  const token = Base64.encode(JSON.stringify({
-      signature:{
-          "publicKey": signObj.key,
-          "sign": signObj.signature
-      }
-      body: msg,
-  }))
+    const token = Base64.encode(JSON.stringify({
+        signature:{
+            "publicKey": signObj.key,
+            "sign": signObj.signature
+        }
+        body: msg,
+    }))
 
     return token;
 }
 
 
 const validateInput = body => {
-  for (const key in body) {
+    for (const key in body) {
 
-    const field = body[key]
+        const field = body[key]
 
-    if(key === 'Expire-Date') {
-      throw new Error('Please do not rewrite "Expire-Date" field');
+        if(key === 'Expire-Date') {
+            throw new Error('Please do not rewrite "Expire-Date" field');
+        }
+
+        if(key === 'Web3-Token-Version') {
+            throw new Error('Please do not rewrite "Web3-Token-Version" field');
+        }
+
+        if(typeof field !== 'string') {
+            throw new Error('Body can only contain string values');
+        }
     }
-
-    if(key === 'Web3-Token-Version') {
-      throw new Error('Please do not rewrite "Web3-Token-Version" field');
-    }
-
-    if(typeof field !== 'string') {
-      throw new Error('Body can only contain string values');
-    }
-  }
 };
 
 const buildMessage = data => {
-  const message = [];
-  for (const key in data) {
-    message.push(`${key}: ${data[key]}`)
-  }
-  return message.join('\n');
+    const message = [];
+    for (const key in data) {
+        message.push(`${key}: ${data[key]}`)
+    }
+    return message.join('\n');
 };
